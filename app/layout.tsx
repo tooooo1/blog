ad7c8a6955a0b266d06dcedc0d2dc411872e02f6
Chung-il Jung <jungchungil7@gmail.com>
2024-09-29 17:24:55 +0900

    blog

---
+++ app/layout.tsx
@@ -0,0 +1,52 @@
+import "./globals.css";
+
+import type { Metadata, Viewport } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "blog 퉁이리",
+  description: "정충일",
+  keywords: ["블로그", "개발", "퉁이리", "정충일"],
+};
+
+export const viewport: Viewport = {
+  themeColor: "transparent",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className="p-4">
+        <header className="flex justify-between">
+          <h1>
+            <Link href="/" className="font-semibold">
+              퉁
+            </Link>
+          </h1>
+          <nav>
+            <ul className="flex gap-4">
+              <li>
+                <Link href="/about">about</Link>
+              </li>
+              <li>
+                <a target="_blank" href="https://www.linkedin.com/in/tooo1">
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="flex flex-col min-h-screen justify-center items-center">
+          {children}
+        </main>
+        <footer className="text-center text-gray-400 text-xs">
+          blog. 퉁이리
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+  },
+});
+
+export default withMDX(nextConfig);
